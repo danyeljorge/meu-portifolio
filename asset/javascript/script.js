7449455bdd3dcc3d26ddb2b2b59a956077a25095
Daniel Jorge <danyeljorge@outlook.com>
2025-02-16 13:44:15 -0300

    Efeitos nos elementos com ScrollReveal

---
+++ asset/javascript/script.js
@@ -0,0 +1,62 @@
+window.revelar = ScrollReveal({ reset: true });
+
+// TOPO DO SITE
+
+revelar.reveal(".efeitoHeroTopo", {
+  duration: 2000,
+  distance: "90px",
+});
+
+revelar.reveal(".efeitoExpertTopo", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+});
+
+revelar.reveal(".efeitoSkil1", {
+  duration: 2000,
+  distance: "90px",
+});
+
+revelar.reveal(".efeitoSkil2", {
+  duration: 2000,
+  distance: "90px",
+  delay: 400,
+});
+
+revelar.reveal(".efeitoSkil3", {
+  duration: 2000,
+  distance: "90px",
+  delay: 800,
+});
+
+revelar.reveal(".efeitoSkil4", {
+  duration: 2000,
+  distance: "90px",
+  delay: 1200,
+});
+
+revelar.reveal(".efeitoSkil5", {
+  duration: 2000,
+  distance: "90px",
+  delay: 1600,
+});
+
+revelar.reveal(".efeitoSkil6", {
+  duration: 2000,
+  distance: "90px",
+  delay: 2000,
+});
+
+revelar.reveal(".efeitoSobreImg", {
+  duration: 2000,
+  distance: "90px",
+  origin: "left",
+});
+
+revelar.reveal(".efeitoSobreText", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+  origin: "right",
+});
