602ab8a9637d7b3cb2a871a27cf6d58e395c86f3
Daniel Jorge <danyeljorge@outlook.com>
2025-02-20 02:31:16 -0300

    suavidade no scroll com lenis

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -111,3 +111,24 @@ textoAnimado.forEach((char) => {
     stagger: 0.1,
   });
 });
+
+// ROLAGEM SUAVE COM LENIS
+
+const lenis = new Lenis({
+  duration: 2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  direction: "vertical", // vertical, horizontal
+  gestureDirection: "vertical", // vertical, horizontal, both
+  smooth: true,
+  mouseMultiplier: 1,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
